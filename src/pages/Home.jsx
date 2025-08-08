16eab437613b1a377536f97e3b5c3e2e71a1e492
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-08-08 10:19:41 -0700

    Create Home.jsx And Render Navbar,Main and Footer.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import Main from "../components/mainDOM";
+
+
+const Home = () => {
+
+    return (
+      <>
+        <Navbar />
+        <Main />
+        <Footer />
+      </>
+    );
+}
+
+export default Home;
