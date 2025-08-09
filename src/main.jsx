739a0838d662a3cebd813633ee5bdf3c1ef4b2a7
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-08-09 15:48:00 -0700

    Update src/main.jsx, Import routes, and CartProvider. - CartProvider for Making Cart data Available global. - Wrap App too With routes to make them Accessible global.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
+import routes from './routes.jsx';
+import { CartProvider } from './context/cartcontext.jsx';
 
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <BrowserRouter>
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
+  <CartProvider>
+    <RouterProvider router={router}>
       <App />
-    </BrowserRouter>
-  // </StrictMode>,
-)
+    </RouterProvider>
+  </CartProvider>
+);
