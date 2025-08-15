e2842698684bbee1ae8cd3a14b41ee890e62b5b7
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-08-14 19:52:45 -0700

    Create Footer.jsx, add Dev's link and Frontend mentor's link in src\components\footer.jsx.

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <>
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://www.frontendmentor.io/profile/LemnsaLeger" target="_blank">@devLemnsa</a>
+          .
+        </div>
+      </>
+    );
+}
+
+export default Footer;
