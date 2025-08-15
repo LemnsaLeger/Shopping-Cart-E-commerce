4707761de3a42e3929fd88d8f62b64edb46e4f96
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-08-14 20:12:09 -0700

    Responsive Styling with Tailwindcss.

--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -42,7 +42,7 @@ const Store = () => {
     return (
       <>
         <Navbar />
-        <div className="grid grid-cols-4 gap-6 p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
           {items.map((product) => (
             <Link
               key={product.id}
