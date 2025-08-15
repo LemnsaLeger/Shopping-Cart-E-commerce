c87f18575516171002090bbdfe2af40ec8f651bc
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-08-14 20:04:21 -0700

    Update cartcontext, Handle saving and retrieving product images from Local Storage.

--- src/context/cartcontext.jsx
+++ src/context/cartcontext.jsx
@@ -1,37 +1,58 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
     const [cartItems, setCartItems] = useState([]);
+    const [showSuccess, setShowSuccess] = useState(false);
+
+    //  adding product to localstorage
+    useEffect(() => {
+         if (!cartItems || cartItems.length === 0) return;
+        let cartData = {} ;
+        cartItems.forEach(item => {
+            cartData[item.name] = item;
+        });
+        localStorage.setItem("cart", JSON.stringify(cartData));
+    },[cartItems]);
+
+    // loading logic
+    useEffect(() => {
+        const data = localStorage.getItem("cart");
+        if(data) {
+            const parsed = JSON.parse(data);
+            const items = Object.values(parsed); // to array
+            setCartItems(items);
+        }
+    },[]);
 
     // add to cart
     const addToCart = (product, qty = 0) => {
         if(!qty && qty < 1) return;
 
-        
+
         setCartItems((prev) => {
-            const existing = prev.find((item) => item.id === product.id);
+            const existing = prev.find((item) => item.name === product.name);
 
             if(existing) {
-                return prev.map((item) => item.id === product.id ? {...item, quantity: item.quantity + qty} : item
+                return prev.map((item) => item.name === product.name ? {...item, quantity: item.quantity + qty} : item
             );
             }
-
             return [...prev, {...product, quantity: qty}];
         });
+        setShowSuccess(true);
     };
 
     // remove from card
-    const removeFromCart = (id) => {
-        setCartItems((prev) => prev.filter((item) => item.id !== id))
+    const removeFromCart = (name) => {
+        setCartItems((prev) => prev.filter((item) => item.name !== name))
     };
 
     // counting cart items
     const cartCount = cartItems.reduce((acc, item) => acc + item.quantity, 0);
 
     return (
-        <CartContext.Provider value={{cartItems, addToCart, removeFromCart, cartCount}}>
+        <CartContext.Provider value={{cartItems, addToCart, removeFromCart, cartCount, showSuccess, setShowSuccess}}>
             {children}
         </CartContext.Provider>
     )
