7a1b176b00088a71b07978ddd5df8a7a8abe7cde
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-08-14 19:47:24 -0700

    Add pageNotFound component to Routes in src\routes.jsx To be displayed for Wrong routes.

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,14 @@ import Store from "./pages/store"
 import Main from "./components/mainDOM"
 import Home from "./pages/Home";
 import Cart from "./pages/cart";
+import PageNotFound from "./pages/pageNotFound";
 
 
 const routes = [
     {
         path: "/",
         element: <Home />,
+        errorElement: <PageNotFound />,
     },
     {
         path: "/store",
